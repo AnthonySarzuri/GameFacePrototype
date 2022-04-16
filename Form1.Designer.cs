e3d4bacd479e6be01bebb7d0eab8a9035358d251
Drazen Cesar Landivar Glasinovic <lgd2024644@est.univalle.edu>
2022-04-16 18:56:45 -0400

    Commit 16/04 18:55 (Drazen Landivar)-->Agregue un picturebox para el logo no tiene funcionalidad ni el logo

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,8 @@ namespace GameFacePrototype
         {
             this.BTNIniciar = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // BTNIniciar
@@ -51,15 +53,25 @@ namespace GameFacePrototype
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(376, 65);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.BTNIniciar);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -68,6 +80,7 @@ namespace GameFacePrototype
 
         private System.Windows.Forms.Button BTNIniciar;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
