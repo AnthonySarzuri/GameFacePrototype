d614c60bc67a7bbb1a2cd6c3032af1a35b3b6454
ANTHONY CALEB SARZURI SEA <ssa2023593@est.univalle.edu>
2022-04-16 18:40:16 -0400

    Commit 16/04 18:37 (Anthony Sarzuri)-->Hea agregado 2 botones uno de inicio de sesion y otro sin nombre y con un id por defecto para mostrar el funcionamiento de Git y Github las herrameintas que nos permitiran trabajar de manera un poco mas profesional, ademas hice un nuevo formulario para que el boton inicio de sesion tenga una accion, presiono "Iniciar" y me muestra un nuevo formulario

--- FormLogin.cs
+++ FormLogin.cs
@@ -10,19 +10,11 @@ using System.Windows.Forms;
 
 namespace GameFacePrototype
 {
-    public partial class Form1 : Form
+    public partial class FormLogin : Form
     {
-        public Form1()
+        public FormLogin()
         {
             InitializeComponent();
-        }
-
-
-        private void button1_Click_1(object sender, EventArgs e)
-        {
-            FormLogin login = new FormLogin();
-            login.Show();
-            this.Hide();
         }
     }
 }
