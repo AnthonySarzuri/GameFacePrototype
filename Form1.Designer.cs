058d9ee03f34c21d5ffcaa6abda3420bc401c7cc
ANTHONY CALEB SARZURI SEA <ssa2023593@est.univalle.edu>
2022-04-16 12:23:33 -0400

    My commit>Anthony

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,7 @@ namespace GameFacePrototype
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -70,11 +71,21 @@ namespace GameFacePrototype
             this.label2.TabIndex = 3;
             this.label2.Text = "HOla";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(131, 257);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -92,6 +103,7 @@ namespace GameFacePrototype
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
