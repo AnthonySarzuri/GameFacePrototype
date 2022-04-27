e666a1a278bf900f25dcff3149965513fc9d042c
Drazen Cesar Landivar Glasinovic <lgd2024644@est.univalle.edu>
2022-04-27 00:09:42 -0400

    Commit 26/04 23:54 (Drazen Landivar)-->Arregle algunas cositas ya funciona mejor

--- PrivateInterface.Designer.cs
+++ PrivateInterface.Designer.cs
@@ -31,11 +31,15 @@ namespace GameFacePrototype
         {
             this.button1 = new System.Windows.Forms.Button();
             this.bntProfile = new System.Windows.Forms.Button();
+            this.panelRight = new System.Windows.Forms.Panel();
+            this.panelLeft = new System.Windows.Forms.Panel();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.panelRight.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(561, 2);
+            this.button1.Location = new System.Drawing.Point(148, 17);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(36, 32);
             this.button1.TabIndex = 2;
@@ -45,7 +49,7 @@ namespace GameFacePrototype
             // 
             // bntProfile
             // 
-            this.bntProfile.Location = new System.Drawing.Point(463, 45);
+            this.bntProfile.Location = new System.Drawing.Point(109, 97);
             this.bntProfile.Name = "bntProfile";
             this.bntProfile.Size = new System.Drawing.Size(75, 23);
             this.bntProfile.TabIndex = 3;
@@ -53,25 +57,59 @@ namespace GameFacePrototype
             this.bntProfile.UseVisualStyleBackColor = true;
             this.bntProfile.Click += new System.EventHandler(this.bntProfile_Click);
             // 
+            // panelRight
+            // 
+            this.panelRight.Controls.Add(this.bntProfile);
+            this.panelRight.Controls.Add(this.button1);
+            this.panelRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelRight.Location = new System.Drawing.Point(1294, 0);
+            this.panelRight.Name = "panelRight";
+            this.panelRight.Size = new System.Drawing.Size(238, 993);
+            this.panelRight.TabIndex = 4;
+            // 
+            // panelLeft
+            // 
+            this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLeft.Location = new System.Drawing.Point(0, 0);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Size = new System.Drawing.Size(302, 993);
+            this.panelLeft.TabIndex = 5;
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.AutoScroll = true;
+            this.mainPanel.AutoSize = true;
+            this.mainPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(302, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(992, 993);
+            this.mainPanel.TabIndex = 6;
+            // 
             // PrivateInterface
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(600, 366);
-            this.Controls.Add(this.bntProfile);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1532, 993);
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.panelLeft);
+            this.Controls.Add(this.panelRight);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "PrivateInterface";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "PrivateInterface";
+            this.panelRight.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button bntProfile;
+        private System.Windows.Forms.Panel panelRight;
+        private System.Windows.Forms.Panel panelLeft;
+        private System.Windows.Forms.Panel mainPanel;
     }
 }
--- Profile.designer.cs
+++ Profile.designer.cs
@@ -28,25 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.PanelPrincipal = new System.Windows.Forms.Panel();
             this.btnNewPost = new System.Windows.Forms.Button();
             this.btnEditProfilePicture = new System.Windows.Forms.Button();
             this.btnComentarioPrueba = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
+            this.panelLeft = new System.Windows.Forms.Panel();
+            this.panelRight = new System.Windows.Forms.Panel();
+            this.PanelPrincipal = new System.Windows.Forms.Panel();
+            this.panelLeft.SuspendLayout();
+            this.panelRight.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // PanelPrincipal
-            // 
-            this.PanelPrincipal.AutoScroll = true;
-            this.PanelPrincipal.AutoSize = true;
-            this.PanelPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.PanelPrincipal.Location = new System.Drawing.Point(200, 0);
-            this.PanelPrincipal.Name = "PanelPrincipal";
-            this.PanelPrincipal.Size = new System.Drawing.Size(1019, 981);
-            this.PanelPrincipal.TabIndex = 21;
             // 
             // btnNewPost
             // 
@@ -78,24 +68,35 @@
             this.btnComentarioPrueba.UseVisualStyleBackColor = true;
             this.btnComentarioPrueba.Click += new System.EventHandler(this.btnComentarioPrueba_Click);
             // 
-            // panel1
+            // panelLeft
             // 
-            this.panel1.Controls.Add(this.btnNewPost);
-            this.panel1.Controls.Add(this.btnComentarioPrueba);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 981);
-            this.panel1.TabIndex = 22;
+            this.panelLeft.Controls.Add(this.btnNewPost);
+            this.panelLeft.Controls.Add(this.btnComentarioPrueba);
+            this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLeft.Location = new System.Drawing.Point(0, 0);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Size = new System.Drawing.Size(200, 981);
+            this.panelLeft.TabIndex = 22;
             // 
-            // panel2
+            // panelRight
             // 
-            this.panel2.Controls.Add(this.btnEditProfilePicture);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel2.Location = new System.Drawing.Point(1019, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 981);
-            this.panel2.TabIndex = 25;
+            this.panelRight.Controls.Add(this.btnEditProfilePicture);
+            this.panelRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelRight.Location = new System.Drawing.Point(1019, 0);
+            this.panelRight.Name = "panelRight";
+            this.panelRight.Size = new System.Drawing.Size(200, 981);
+            this.panelRight.TabIndex = 25;
+            // 
+            // PanelPrincipal
+            // 
+            this.PanelPrincipal.AutoScroll = true;
+            this.PanelPrincipal.AutoSize = true;
+            this.PanelPrincipal.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.PanelPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PanelPrincipal.Location = new System.Drawing.Point(200, 0);
+            this.PanelPrincipal.Name = "PanelPrincipal";
+            this.PanelPrincipal.Size = new System.Drawing.Size(819, 981);
+            this.PanelPrincipal.TabIndex = 26;
             // 
             // Profile
             // 
@@ -103,24 +104,24 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(1219, 981);
-            this.Controls.Add(this.panel2);
             this.Controls.Add(this.PanelPrincipal);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelRight);
+            this.Controls.Add(this.panelLeft);
             this.Name = "Profile";
             this.Text = "Form1";
-            this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
+            this.panelLeft.ResumeLayout(false);
+            this.panelRight.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Panel PanelPrincipal;
         private System.Windows.Forms.Button btnComentarioPrueba;
         private System.Windows.Forms.Button btnEditProfilePicture;
         private System.Windows.Forms.Button btnNewPost;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panelLeft;
+        private System.Windows.Forms.Panel panelRight;
+        private System.Windows.Forms.Panel PanelPrincipal;
     }
 }
