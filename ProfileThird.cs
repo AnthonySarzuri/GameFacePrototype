20adb2df638464fbba20cec427f16baad9b9ac36
LeoGMR20 <leo250703@gmail.com>
2022-04-28 10:15:25 -0400

    Commit 28/04/2022 10:10 (Leandro Morales) --> Añadí la conexion al procedimiento almacenado de mostrar usuario por id al perfil de tercera persona.

--- ProfileThird.cs
+++ ProfileThird.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,11 +17,44 @@ namespace GameFacePrototype
         public ProfileThird()
         {
             InitializeComponent();
+            try
+            {
+                showUserData();
+            }
+            catch (Exception E) 
+            {
+                MessageBox.Show("Ocurrió un error.");
+            }
         }
 
         private void BTNBack_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+        private void showUserData() 
+        {
+            DataTable dt = new DataTable();
+            string sConexion = "Data Source=SQL8001.site4now.net;Initial Catalog=db_a85e89_gfdb;User Id=db_a85e89_gfdb_admin;Password=l05tvcvs";
+            SqlConnection dataConnection = new SqlConnection(sConexion);
+            SqlDataAdapter da = new SqlDataAdapter("SP_ShowUserId", dataConnection);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+            da.SelectCommand.Parameters.Add("@idUser", SqlDbType.NVarChar, 50);
+            if (dt.Rows.Count > 0)
+            {
+                LBLShowUserId.Text = dt.Rows[0]["IdUser"].ToString();
+                LBLShowUser.Text = dt.Rows[0]["UserName"].ToString();
+                LBLShowBirthday.Text = dt.Rows[0]["Birthday"].ToString();
+                LBLShowBiography.Text = dt.Rows[0]["Biography"].ToString();
+                Byte[] myByte = new byte[0];
+                myByte = (Byte[])(dt.Rows[0]["ProfilePhoto"]);
+                MemoryStream ms = new MemoryStream(myByte);
+                PBProfilePicture.Image = Image.FromStream(ms);
+            }
+            else
+            {
+                MessageBox.Show("Datos No Encontrados");
+
+            }
         }
     }
 }
