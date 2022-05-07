88a1ec638fec3e4e64b0ff317bc183b2dd495812
AnthonySarzuri <ssa2023593@est.univalle.edu>
2022-05-07 11:05:57 -0400

    Commit Anthony --> botnones

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -32,13 +32,13 @@ namespace GameFacePrototype
             this.components = new System.ComponentModel.Container();
             this.tbUser = new System.Windows.Forms.TextBox();
             this.tbPassword = new System.Windows.Forms.TextBox();
-            this.btnLogin = new System.Windows.Forms.Button();
             this.lblPasswordEmpty = new System.Windows.Forms.Label();
             this.lblUserEmpty = new System.Windows.Forms.Label();
             this.lblError = new System.Windows.Forms.Label();
             this.lblRegister = new System.Windows.Forms.Label();
             this.lblPasswordGet = new System.Windows.Forms.Label();
             this.timerOut = new System.Windows.Forms.Timer(this.components);
+            this.btnLogin = new GameFacePrototype.Personalizado.Boton();
             this.SuspendLayout();
             // 
             // tbUser
@@ -55,16 +55,6 @@ namespace GameFacePrototype
             this.tbPassword.PasswordChar = '*';
             this.tbPassword.Size = new System.Drawing.Size(189, 20);
             this.tbPassword.TabIndex = 1;
-            // 
-            // btnLogin
-            // 
-            this.btnLogin.Location = new System.Drawing.Point(328, 357);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(100, 23);
-            this.btnLogin.TabIndex = 2;
-            this.btnLogin.Text = "Iniciar Sesion";
-            this.btnLogin.UseVisualStyleBackColor = true;
-            this.btnLogin.Click += new System.EventHandler(this.button1_Click);
             // 
             // lblPasswordEmpty
             // 
@@ -115,17 +105,36 @@ namespace GameFacePrototype
             this.timerOut.Interval = 1000;
             this.timerOut.Tick += new System.EventHandler(this.timerOut_Tick);
             // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnLogin.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnLogin.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnLogin.BorderRadius = 20;
+            this.btnLogin.BorderSize = 0;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(302, 356);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(150, 40);
+            this.btnLogin.TabIndex = 9;
+            this.btnLogin.Text = "boton1";
+            this.btnLogin.TextColor = System.Drawing.Color.White;
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(794, 496);
+            this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.lblPasswordGet);
             this.Controls.Add(this.lblRegister);
             this.Controls.Add(this.lblError);
             this.Controls.Add(this.lblUserEmpty);
             this.Controls.Add(this.lblPasswordEmpty);
-            this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.tbPassword);
             this.Controls.Add(this.tbUser);
             this.Name = "Login";
@@ -139,12 +148,12 @@ namespace GameFacePrototype
 
         private System.Windows.Forms.TextBox tbUser;
         private System.Windows.Forms.TextBox tbPassword;
-        private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Label lblPasswordEmpty;
         private System.Windows.Forms.Label lblUserEmpty;
         private System.Windows.Forms.Label lblError;
         private System.Windows.Forms.Label lblRegister;
         private System.Windows.Forms.Label lblPasswordGet;
         private System.Windows.Forms.Timer timerOut;
+        private Personalizado.Boton btnLogin;
     }
 }
--- Login.cs
+++ Login.cs
@@ -119,5 +119,62 @@ namespace GameFacePrototype
 
             Global.Status = 1;
         }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            string sConexion = Global.Conexion;
+
+            SqlConnection dataConnection = new SqlConnection(sConexion);
+            SqlDataAdapter da = new SqlDataAdapter("SP_LOGIN", dataConnection);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+            try
+            {
+                da.SelectCommand.Parameters.Add("@NickNameorEmail", SqlDbType.NVarChar, 50);
+                da.SelectCommand.Parameters.Add("@Password", SqlDbType.NVarChar, 50);
+
+                da.SelectCommand.Parameters["@NickNameorEmail"].Value = tbUser.Text;
+                da.SelectCommand.Parameters["@Password"].Value = tbPassword.Text;
+
+                da.Fill(dt);
+                if (dt.Rows.Count >= 1)
+                {
+                    //Aqui di la vuelata la parte donde se declara el Login y la parte donde se declara la pantalla de PrivateInterface 
+                    //para que el login se declare antes de que se habra la siguiente pantalla lo necesitaba para que funcionen los posts
+                    //pero si necesitan que este al reves por alguna razón entonces solo vuelvanlo a poner debajo eso es todo xd
+                    int iduser;
+                    iduser = (int)(dt.Rows[0][0]);
+                    Global.IdUser = iduser;
+
+                    conectedUP();
+
+                    PrivateInterface inter = new PrivateInterface();
+                    inter.Show();
+                    this.Hide();
+
+                }
+                else
+                {
+                    lblError.Text = "Su usuario o contraseña son incorrectos, revise por favor";
+                    tbPassword.Text = "";
+                    Global.tries++;
+                }
+                if (Global.tries >= 3)
+                {
+                    MessageBox.Show("Número de intentos de inicio de sesión superado.");
+                    btnLogin.Enabled = false;
+                    timerOut.Start();
+                    //Global.sec = 5;
+                    Global.cont = 0;
+                }
+
+
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show("Ha ocurrido un error", "Lo Sentimos :(");
+            }
+        }
     }
 }
